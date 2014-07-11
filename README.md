a7ef345cccec173109961c425a1552a281f1a9cf
unknown <martin@MartinN-PC.entelect.local>
2014-07-11 13:16:17 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+#kwaai-mongo
+
+Node helpers for mongoDB
+
+##Description
+A set of helper tools to assist in working with mongo in Node
+
+##API
+###connectToCollection(name,connectionString,callback)
+
+
+###connectToCollections(connectionString,callback,collectionName1,collectionName2,...)
+
+
+###buildQueryFromOptions(query,options)
+
---
+++ testing.js
@@ -0,0 +1,18 @@
+var kwaaiSchema=require('./lib/schema.js');
+
+var schema={
+    $schema: "http://json-schema.org/draft-04/schema#",
+    title: "test",
+    description:"test",
+    properties: {
+        name: {type: "string", pattern: "^[a-z0-9_\-]+$"},
+        description: {type: "string"}
+    }
+}
+
+var doc={
+   name:"abc",
+   description:"test"
+}
+var invalid=kwaaiSchema.validateToSchema(doc,schema);
+console.log(invalid)
